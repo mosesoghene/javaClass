efaa32881e8e23c52021fb6ad1cbac63236a082f
mosesoghene <mosesogheneo@gmail.com>
2024-10-28 05:41:07 +0100

    leap year finder program

---
+++ assignments/oct-28-24/LeapYear.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class LeapYear {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("enter the year \n> ");
+		int year = scanner.nextInt();
+
+		if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+			System.out.printf("%d is a leap year%n", year);
+		} else {
+			System.out.printf("%d is not a leap year%n", year);
+		}
+	}
+}
