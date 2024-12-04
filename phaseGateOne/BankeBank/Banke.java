8db1211592defc346753c7b52f4e6127762b451e
Moses Oghene <mosesogheneo@gmail.com>
2024-12-04 13:11:49 +0100

    PhaseGate BankeBank - changePin, getFullName, getBalance methods tests fine

--- phaseGateOne/BankeBank/Banke.java
+++ phaseGateOne/BankeBank/Banke.java
@@ -30,6 +30,15 @@ class Account{
     this.balance += amount;
   }
   
+  public String withdraw(double amount){
+    if (amount < this.balance){
+      this.balance -= amount;
+      return "Withdrawal successful";
+    } else {
+      return "Insufficient account balance";
+    }
+  }
+  
   public double getBalance(){
     return this.balance;
   }
@@ -38,4 +47,24 @@ class Account{
     return this.accountNumber;
   }
   
+  public String getFirstName(){
+    return this.firstName.toUpperCase();
+  }
+  
+  public String getLastName(){
+    return this.lastName.toUpperCase();
+  }
+  
+  public String getFullName(){
+    return getFirstName() + " " + getLastName();
+  }
+  
+  public String changePin(String oldPin, String newPin){
+    if (oldPin.equals(this.pin)){
+      this.pin = newPin;
+      return "Pin successfully changed";
+    } else{      
+      return "Old pin incorrect";
+    }
+  }  
 }
--- phaseGateOne/BankeBank/TestBanke.java
+++ phaseGateOne/BankeBank/TestBanke.java
@@ -23,4 +23,35 @@ public class TestBanke{
     String accountNumber = account.getAccountNumber();
     assertTrue(accountNumber.length() == 10);
   }
+  
+  @Test
+  public void testThatWithdrawalReturnsSuccessForWithdrawalsLowerThanBalance(){
+    Account account = new Account("Moses", "Oghene", "1234");    
+    account.deposit(1203.4);
+    String response = account.withdraw(500);
+    assertEquals(response, "Withdrawal successful");
+  }
+  
+  @Test
+  public void testThatWithdrawalReturnsFailureForWithdrawalsHigherThanBalance(){
+    Account account = new Account("Moses", "Oghene", "1234");    
+    account.deposit(1203.4);
+    
+    String response = account.withdraw(5000);
+    assertEquals(response, "Insufficient account balance");
+  }
+  
+  @Test
+  public void testThatChnagePinReturnsFailureForIncorrectOldPin(){
+    Account account = new Account("Moses", "Oghene", "1234");    
+    String response = account.changePin("1243", "1245");
+    assertEquals(response, "Old pin incorrect");
+  }
+  
+  @Test
+  public void testThatChangePinReturnsSuccessForCorrectOldPin(){
+    Account account = new Account("Moses", "Oghene", "1234");    
+    String response = account.changePin("1234", "1245");
+    assertEquals(response, "Pin successfully changed");
+  }
 }
