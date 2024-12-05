82f1ffc662debdfa27a28274de1695c15d9b892c
Moses Oghene <mosesogheneo@gmail.com>
2024-12-05 09:02:36 +0100

    PhaseGate Phonebook - Java ✅️

--- phaseGateOne/BankeBank/Phonebook.java
+++ phaseGateOne/BankeBank/Phonebook.java
@@ -31,7 +31,10 @@ public class Contact{
   }
   
   public String getContactDetails(){
-    return "Firstname: " + getFirstName() + "\nLastname : " + getLastName() + "\nPhone Number: " getPhoneNumber() + "\nEmail : " getEmail();
+    return "Firstname: " + getFirstName() + 
+           "\nLastname : " + getLastName() + 
+           "\nPhone Number: " getPhoneNumber() + 
+           "\nEmail : " getEmail() + "\n\n";
   }
   
   public boolean updateFirstName(String firstName){
@@ -63,12 +66,59 @@ public class Phonebook{
     3. Update contact infos
     4. Remove contact
     5. Exit app
-    """;
+    >   """;
     
-    System.out.print
+    System.out.print(menu);
     String option = input.next();
     
+    switch (option) {
+      case "1" -> {
+        
+        clearScreen();
+        System.out.println("*** Create New Contact ***\n")
+        inputPrmpt("Enter firstname: ");
+        String firstName = input.next()
+        
+        inputPrmpt("Enter phone number: ");
+        String phoneNumber = input.next()
+        
+        contacts.add(new Contact(firstName, phoneNumber));
+        mainMenu();
+      }
+      
+      case "2" -> {
+        clearScreen();
+        System.out.println("*** Search Contacts ***\n")
+        inputPrompt("Enter name or phone number");
+        String searchKeyword = input.next();
+        clearScreen();
+        System.out.println("Search for '" + searchKeyword +"'")
+        for (contact: contacts){
+          if (
+            contact.getFirstName().contains(searchKeyword) || 
+            contact.getLastName().contains(searchKeyword) ||
+            contact.getPhoneNumber().contains(searchKeyword) ||
+            contact.getEmail().contains(searchKeyword)
+            ){
+            System.out.println(contact.getContactDetails());            
+          }
+        }
+        mainMenu();
+      }
+      case "3" -> {        
+        System.out.println("*** Search Contacts ***\n")
+      }
+    }
   }
 
+
+  private void inputPrompt(String text){
+    System.out.print(text);
+  }
+  
+  private void clearScreen() {  
+    System.out.print("\033[H\033[2J");  
+    System.out.flush();  
+  }
 }
 
