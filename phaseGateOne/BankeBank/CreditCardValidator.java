424af00c762f476a30afd77b8daabfa7396b225d
Moses Oghene <mosesogheneo@gmail.com>
2024-12-05 05:21:58 +0100

    PhaseGate CreditCardValidator - isValidCard ✅️

--- phaseGateOne/BankeBank/CreditCardValidator.java
+++ phaseGateOne/BankeBank/CreditCardValidator.java
@@ -20,5 +20,31 @@ public class CreditCardValidator {
     }
     return "Invalid Card Type";
   }
+  
+  public static boolean isValidCard(String cardNumber) {
+    int sumOfDoubleEvenPlace = sumOfDoubleEvenPlace(cardNumber);
+    int sumOfOddPlace = sumOfOddPlace(cardNumber);
+    int totalSum = sumOfDoubleEvenPlace + sumOfOddPlace;
+    
+    return totalSum % 10 == 0;
+  }
+  
+  public static int sumOfDoubleEvenPlace(String cardNumber) {
+    int sum = 0;
+    for (int i = cardNumber.length() - 2; i >= 0; i -= 2) {
+      int digit = Character.getNumericValue(cardNumber.charAt(i));
+      int doubledDigit = digit * 2;
+      sum += doubledDigit > 9 ? (doubledDigit / 10 + doubledDigit % 10) : doubledDigit;
+    }
+    return sum;
+  }
+    
+    public static int sumOfOddPlace(String cardNumber) {
+      int sum = 0;
+      for (int i = cardNumber.length() - 1; i >= 0; i -= 2) {
+        sum += Character.getNumericValue(cardNumber.charAt(i));
+      }
+      return sum;
+  }
 }
 
--- phaseGateOne/BankeBank/TestCreditCardValidator.java
+++ phaseGateOne/BankeBank/TestCreditCardValidator.java
@@ -3,7 +3,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class TestCreditCardValidator{
   @Test
-  public void testThatIsValidReturnCorrectResponse(){
+  public void testThatIsValidLengthReturnCorrectResponse(){
     CreditCardValidator creditCard = new CreditCardValidator();
     assertTrue(creditCard.isValidLength("12345095678735"));
   }
@@ -12,6 +12,12 @@ public class TestCreditCardValidator{
   public void testThatDetermineCardTypeReturnCorrectResult(){
     CreditCardValidator creditCard = new CreditCardValidator();
     assertEquals(creditCard.determineCardType("12345095678735"), "Invalid Card Type");
+  }
+  
+  @Test
+  public void testThatIsValidCardReturnsCorrectResponse(){
+    CreditCardValidator creditCard = new CreditCardValidator();
+    assertFalse(creditCard.isValidCard("12345095678735"));
   }
 
 }
