db8eb74d89b60b6cafb13e40da14fd3550c8a29d
Moses Oghene <mosesogheneo@gmail.com>
2025-01-21 16:58:31 +0100

    new instance of MyArrayList created successfully

--- DSA/test/dsa/MyArrayListTest.java
+++ DSA/test/dsa/MyArrayListTest.java
@@ -1,9 +1,13 @@
 package dsa;
 
 import dsa.MyArrayList.MyArrayList;
+import org.junit.jupiter.api.BeforeEach;
 
 public class MyArrayListTest {
     MyArrayList strings;
 
-
+    @BeforeEach
+    public void setUp() {
+        strings = new MyArrayList();
+    }
 }
