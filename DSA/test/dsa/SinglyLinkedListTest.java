abbe3559a6716eac6acab1b5609746d489fc76fd
Moses Oghene <mosesogheneo@gmail.com>
2025-01-23 08:42:42 +0100

    SinglyLinkedList : object can be created

---
+++ DSA/test/dsa/SinglyLinkedListTest.java
@@ -0,0 +1,15 @@
+package dsa;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class SinglyLinkedListTest {
+    SinglyLinkedList ssl;
+    @BeforeEach
+    void setUp() {
+        ssl = new SinglyLinkedList();
+    }
+
+
+
+}
