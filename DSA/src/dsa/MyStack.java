652c91b70e82d462a33535c1726a5636cdb143a1
Moses Oghene <mosesogheneo@gmail.com>
2025-01-23 08:22:45 +0100

    StackTest : pop(), isEmpty()

--- DSA/src/dsa/MyStack.java
+++ DSA/src/dsa/MyStack.java
@@ -7,8 +7,21 @@ public class MyStack {
         stack.add(value);
     }
 
-
     public int size() {
         return stack.size();
+    }
+
+    public String pop() throws Exception {
+        String element = stack.get(size() - 1);
+        stack.remove(element);
+        return element;
+    }
+
+    public String peek() throws Exception {
+        return stack.get(size() - 1);
+    }
+
+    public boolean isEmpty() {
+        return stack.isEmpty();
     }
 }
--- DSA/test/dsa/StackTest.java
+++ DSA/test/dsa/StackTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class StackTest {
     private MyStack stack;
@@ -18,5 +19,30 @@ public class StackTest {
         stack.push("One");
         stack.push("Two");
         assertEquals(2, stack.size());
+    }
+
+    @Test
+    public void popXY_sizeDecreases_testPop() throws Exception {
+        stack.push("One");
+        stack.push("Two");
+        assertEquals(2, stack.size());
+        assertEquals("Two",stack.pop());
+    }
+
+    @Test
+    public void pushXY_peekReturnY_peekTest() throws Exception {
+        stack.push("One");
+        stack.push("Two");
+        assertEquals("Two",stack.peek());
+    }
+
+    @Test
+    public void pushXY_popYX_isEmptyReturnsTrue_isEmptyTest() throws Exception {
+        stack.push("One");
+        stack.push("Two");
+        assertEquals(2, stack.size());
+        assertEquals("Two",stack.pop());
+        assertEquals("One", stack.pop());
+        assertTrue(stack.isEmpty());
     }
 }
